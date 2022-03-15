59666b6affa13466f8baae441d6d04fcb0fd9605
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-15 22:59:49 +0200

    adding backend with api request

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const express = require('express')
+
+const server = express()
+const userData = JSON.parse(fs.readFileSync(`../public/data/user.json`))
+server.get('/api', (req, res) => {
+  res.status(200).json({
+    data: {
+      api: userData,
+    },
+  })
+})
+const port = 3001
+server.listen(port, () => {
+  console.log(`App running on port ${port}...`)
+})
