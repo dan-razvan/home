7198f0f994796c99a8fd92073ae09cdbdf88568b
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-16 17:54:43 +0200

    updated with async readFile

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,6 @@ const port = 3001
 server.listen(port, () => {
   console.log(`App running on port ${port}...`)
 })
-// const userData = JSON.parse(fs.readFileSync(`../public/data/user.json`))
-// server.get('/api/user', (req, res) => {
-//   res.status(200).json({
-//     data: {
-//       api: userData,
-//     },
-//   })
-// })
-
-// const userData = console.log(userData)
-
-// console.log(userData)
 
 server.get('/api/user', (req, res) => {
   fs.readFile(`../public/data/user.json`, (err, data) => {
@@ -33,7 +21,3 @@ server.get('/api/user', (req, res) => {
     }
   })
 })
-
-// fs.readFile(`../public/data/user.json`, (err, data) => {
-
-// })
