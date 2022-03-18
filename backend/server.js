b3d9992f04bc284efd0ad2460672880f780d5b8e
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-18 17:39:32 +0200

    updating app front

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ server.listen(port, () => {
   console.log(`App running on port ${port}...`)
 })
 
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
+  next()
+})
 server.get('/api/user', (req, res) => {
   fs.readFile(`../public/data/user.json`, (err, data) => {
     res.status(200).json({
--- src/components/model/User.js
+++ src/components/model/User.js
@@ -12,7 +12,7 @@ class User {
   }
   //dynamic / non-static
   static get() {
-    return fetch('/data/user.json')
+    return fetch('http://localhost:3001/api/user')
       .then((response) => response.json())
       .then((data) => new User(data.nickname, data.avatar, data.lastOnline))
   }
